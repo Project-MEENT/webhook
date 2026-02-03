c9d5bb546014c9127c7645fd3443d161760bcd0e
Ben Peachey <potherca@gmail.com>
2026-02-03 14:20:54 +0100

    Add basic webhook endpoint with method handling and placeholder HTML.

--- index.php
+++ index.php
@@ -12,16 +12,77 @@
  * It should also, where possible, mirror the behavior of the real endpoint (see the Solid Specs for more info).
  */
 
-// Receive incoming data
+ob_start();
 
-// Check Authentication
+$response = [
+    'body' => '',
+    'headers' => [],
+    'status' => 200,
+];
 
-// @TODO: Convert to Linked-Data once ontology is decided upon
+switch ($_SERVER['REQUEST_URI'] ?? '') {
+    case '/':
+        $fileStream = fopen(__FILE__, 'rb');
+        fseek($fileStream, __COMPILER_HALT_OFFSET__);
+        $response['body'] = stream_get_contents($fileStream);
+        break;
+    case '/favicon.ico':
+        header('Location: https://favicon-emoji.pother.ca/%F0%9F%AA%9D');
+        break;
+    default:
+        switch ($_SERVER['REQUEST_METHOD'] ?? '') {
+            case 'GET':
+            case 'PATCH':
+            case 'PUT':
+                $response['status'] = 405;
+                $response['body'] = 'Method not allowed, MUST be POST';
+                break;
 
-// Check which Solid Pod to write to
+            case 'HEAD':
+            case 'OPTIONS':
+                $response['headers'][] = 'Access-Control-Allow-Methods: OPTIONS, HEAD, POST';
+                $response['status'] = 204;
+                break;
 
-// Connect to Solid Pod (using ?)
+            case 'POST':
+                // Receive incoming data
 
-// Write data to Solid Pod
+                // Check Authentication
 
-// Output response
+                // @TODO: Convert to Linked-Data once ontology is decided upon
+
+                // Check which Solid Pod to write to
+
+                // Connect to Solid Pod (using ? see Solid Specs)
+
+                // Write data to Solid Pod (@TODO: Decide on path / resource container)
+                break;
+        }
+
+        break;
+}
+
+$output = ob_get_clean();
+
+if ($output) {
+    //Assume error
+    // @TODO: Add error handling. What should the response be?
+}
+
+http_response_code($response['status']);
+
+array_walk($response['headers'], function ($header) {
+    header($header);
+});
+
+echo trim($response['body']);
+die();
+
+__halt_compiler();
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<title>🕸️🪝</title>
+<style>html {background: black}h1 {text-align: center}</style>
+<h1>🕸️🪝</h1>
+</html>
