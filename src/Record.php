f9bfdf9d966eeb0e0f6883f881d835eb41abd5c6
Ben Peachey <ben@muze.nl>
2026-05-15 15:36:37 +0200

    Add Record class for RDF data conversion.

---
+++ src/Record.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Meent\WebHook;
+
+use EasyRdf\Graph;
+use EasyRdf\Literal\DateTime as RdfDateTime;
+use EasyRdf\RdfNamespace;
+
+class Record
+{
+    public const NAMESPACE = 'http://meent.dev.muze.nl/rdf/';
+    private const PREFIX = 'meent';
+
+    private Graph $graph;
+
+    final public function __construct(Graph $graph)
+    {
+        $this->graph = $graph;
+    }
+
+    final public function populate(array $record, $prefix = ''): Graph
+    {
+        if (! RdfNamespace::get(self::PREFIX)) {
+            RdfNamespace::set(self::PREFIX, self::NAMESPACE . '#');
+        }
+
+        return $this->convertRecordToRdf($this->graph, $record, $prefix);
+    }
+
+    private function convertRecordToRdf($graph, array $record, $prefix = ''): Graph
+    {
+        $resourceUrl = $graph->getUri();
+
+        if (! RdfNamespace::get($prefix)) {
+            RdfNamespace::set($prefix, $resourceUrl . '#');
+        }
+
+        $resource = $graph->resource($resourceUrl, self::PREFIX . ':Record');
+
+        foreach ($record as $key => $value) {
+            $rdfValue = $this->convertValueToRdf($key, $value);
+            $graph->addLiteral($resource, "$resourceUrl#$key", $rdfValue);
+        }
+
+        return $graph;
+    }
+
+    private function convertValueToRdf($key, $value)
+    {
+        switch ($key) {
+            case 'timestamp':
+                if (is_numeric($value)) {
+                    $value = '@' . $value;
+                }
+                $dateTime = new \DateTimeImmutable($value);
+                $rdfValue = new RdfDateTime($dateTime);
+            break;
+
+            default:
+                $rdfValue = $value;
+            break;
+        }
+
+        return $rdfValue;
+    }
+}
