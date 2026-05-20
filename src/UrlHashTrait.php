3c112f9a7e5ded969d3421e769919bf62a7be95b
Ben Peachey <ben@muze.nl>
2026-05-20 18:19:45 +0200

    Add UrlHashTrait::normalizeUrl() method to exposed URL normalization logic.

--- src/UrlHashTrait.php
+++ src/UrlHashTrait.php
@@ -2,20 +2,32 @@
 
 namespace Meent\WebHook;
 
+use Meent\WebHook\Exception\RuntimeException;
+
 trait UrlHashTrait
 {
     final public function hashUrl($url, $algorithm)
     {
+        $data = $this->normalizeUrl($url);
+
+        return hash($algorithm, $data);
+    }
+
+    public function normalizeUrl($url): string
+    {
+        if (! filter_var((string) $url, FILTER_VALIDATE_URL)) {
+            throw RuntimeException::create("Provided URL '$url' is not valid");
+        }
+
         $parts = parse_url($url);
 
         $data = $parts['scheme'] . ($parts['scheme'] === 'http' ? 's' : '')
             . '://'
             . $parts['host']
             . (array_key_exists('port', $parts) ? ':' . $parts['port'] : '')
-            . (array_key_exists('path', $parts) ? rtrim($parts['path'], '/') : '');
+            . (array_key_exists('path', $parts) ? rtrim($parts['path'], '/') : '')
+        ;
 
-        $data = strtolower($data);
-
-        return hash($algorithm, $data);
+        return strtolower($data);
     }
 }
